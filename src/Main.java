2b2ffd89070ece73cbafd897eb8fc6941b835730
Girish Chintakayala <girishb2bsoftech@gmail.com>
2026-08-18 20:01:19 +0530

    First commit

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.util.concurrent.*;
+
+public class Main {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        System.out.println("Hello world!");
+
+        ExecutorService service = Executors.newFixedThreadPool(3);
+
+        Future<Integer> future1 = service.submit(()->{
+            Thread.sleep(1000);
+            return 100;
+        });
+        Future<Integer> future2 = service.submit(()->{
+            Thread.sleep(2000);
+            return 200;
+        });
+        Future<Integer> future3 = service.submit(()->{
+            Thread.sleep(3000);
+            throw new RuntimeException("Some issue with task");
+
+        });
+
+        try
+        {
+            Integer result1 = future1.get();
+            System.out.println(result1);
+            Integer result2 = future2.get();
+            System.out.println(result2);
+            Integer result3 = future3.get();
+            System.out.println(result3);
+
+        }
+        catch (ExecutionException e)
+        {
+            System.out.println("A task failed! Reason :"+e.getMessage());
+        }
+    }
+}
